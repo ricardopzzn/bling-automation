c33d75c5d578bd2a9cb9aa35484d4a377b35a881
Ricardo <ricardopzzn@proton.me>
2026-02-19 13:08:01 -0400

    removendo import que não seram utilizados

--- main.py
+++ main.py
@@ -9,8 +9,6 @@ from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support import expected_conditions as EC
-from selenium.webdriver.common.action_chains import ActionChains
-from selenium.common.exceptions import StaleElementReferenceException
 from webdriver_manager.chrome import ChromeDriverManager
 
 
